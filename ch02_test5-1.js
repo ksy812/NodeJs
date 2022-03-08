16c630c3276fa1415cf9ca0aad1c90c802f4a6b0
김소연 <s2023@e-mirim.hs.kr>
2022-03-08 15:06:48 +0900

    add file(ch02_test6.js) and npm

--- ch02_test5-1.js
+++ ch02_test5-1.js
@@ -1,5 +1,6 @@
 var Calc = require("./calc22");
-console.log(Calc.add(10, 5));
-console.log(Calc.sub(10, 5));
-console.log(Calc.multiply(10, 5));
-console.log(Calc.divide(10, 5));
+console.log("3105 김소연");
+console.log("a + b = " + Calc.add(10, 5));
+console.log("a - b = " + Calc.sub(10, 5));
+console.log("a * b = " + Calc.multiply(10, 5));
+console.log("a / b = " + Calc.divide(10, 5));
