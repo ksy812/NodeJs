33cd011ef9013caefcab240a4bf3e9c8118f9dfe
김소연 <s2023@e-mirim.hs.kr>
2022-03-15 15:16:55 +0900

    RENAME ch03_test11

--- ch02_test5-1.js
+++ ch02_test5-1.js
@@ -1,5 +1,5 @@
 var Calc = require("./calc22");
-console.log("3105 김소연");
+console.log("김소연의 사칙연산");
 console.log("a + b = " + Calc.add(10, 5));
 console.log("a - b = " + Calc.sub(10, 5));
 console.log("a * b = " + Calc.multiply(10, 5));
